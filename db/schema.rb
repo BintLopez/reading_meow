65ce572f18860e24d7926a3b36fa740f0d7b1605
BintLopez <nmlopez21@gmail.com>
2018-03-30 15:03:59 -0500

    run migrations for contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330195827) do
+ActiveRecord::Schema.define(version: 20180330195906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,20 @@ ActiveRecord::Schema.define(version: 20180330195827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cats_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "phone_number"
+    t.string "street_address_1"
+    t.string "street_address_2"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "contactable_type"
+    t.bigint "contactable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contactable_type", "contactable_id"], name: "index_contacts_on_contactable_type_and_contactable_id"
   end
 
   create_table "libraries", force: :cascade do |t|
