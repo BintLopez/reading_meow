4f634d3414c73778bb421e5de8024d4b93752f48
BintLopez <nmlopez21@gmail.com>
2018-03-31 16:59:10 -0500

    pull out new user registration form into a partial with the help of my tests

--- spec/views/devise/registrations/new.html.erb_spec.rb
+++ spec/views/devise/registrations/new.html.erb_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "devise/registrations/new", type: :view do
+	it "displays the sign up title" do
+		render
+		expect(rendered).to include("Sign Up")
+	end
 
   it "renders the correct sign up form" do
     render
     expect(rendered).to match /a cat who wants some books!/
+  end
+
+  it "renders the text and link to login to an existing account" do
+  	render
+  	expect(rendered).to match /Already have an account?/
+  	expect(rendered).to include('<a href="/login">Log In</a>')
   end
 
 end
