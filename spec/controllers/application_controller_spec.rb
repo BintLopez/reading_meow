bedc7e49d55f15e99ef0c5da4e0fe0e1455a9509
BintLopez <nmlopez21@gmail.com>
2018-03-31 13:34:58 -0500

    set up devise test helper

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
 
+  before { login_with }
+
   describe "GET #home" do
     it "returns a success response" do
       get :home
