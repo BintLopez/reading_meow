fc9223aefc045970a205d93a82c3cb619b7d85ee
BintLopez <nmlopez21@gmail.com>
2018-03-31 09:28:14 -0500

    add has_one cat reading wrangler to user + test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
-
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :cat_reading_wrangler
   has_many :cats
   has_many :contacts, as: :contactable
 end
-
-
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to have_many(:cats) }
   it { is_expected.to have_many(:contacts) }
+  it { is_expected.to have_one(:cat_reading_wrangler) }
 
   it 'works' do
   end
