2523d811e1cd5067a083ca1385ee6a01144c0ffa
BintLopez <nmlopez21@gmail.com>
2018-03-31 17:06:26 -0500

    successfully render devise registration partial from home page -- tests prove it

--- spec/views/application/home.html.erb_spec.rb
+++ spec/views/application/home.html.erb_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "application/home", type: :view do
     expect(rendered).to match /Welcome to Reading Meow/
   end
 
+  it "renders the new account sign up form" do
+  	render
+  	expect(rendered).to include("I have a library card and want to bring the gift of reading to cats!")
+  end
 end
-
-
