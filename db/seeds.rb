f23dc61783da893630cb02985fba8371594926cb
BintLopez <nmlopez21@gmail.com>
2018-03-30 20:34:41 -0500

    fix factory and seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,21 @@
-# Welllll....
+require "factory_bot"
+require "faker"
 
-# We were going to use this file to seed some
-# helpful data into our development db, but
-# I'm getting an error when trying to seed my db
-# with users!
+5.times do
+	user = FactoryBot.build(:user, email: Faker::Internet.email, password: 'password')
+	user.save(validate: false)
+	FactoryBot.create(:cat, user: user)
+	FactoryBot.create(:contact, contactable: user)
+end
 
-# Commented on a closed rails issue here:
-# https://github.com/rails/rails/issues/18407#issuecomment-377653442
+5.times do
+	user = FactoryBot.build(:user, email: Faker::Internet.email, password: 'password')
+	user.save(validate: false)
+	FactoryBot.create(:cat_reading_wrangler, user: user)
+	FactoryBot.create(:contact, contactable: user)
+end
 
-# Can you reproduce?
-# comment in the following code, save, and run `rake db:seed`
-
-# require "factory_bot"
-# require "faker"
-
-# 5.times do
-# 	user = FactoryBot.create(:user)
-# 	FactoryBot.create(:cat, user: user)
-# 	FactoryBot.create(:contact, contactable: user)
-# end
-
-# 5.times do
-# 	user = FactoryBot.create(:user)
-# 	FactoryBot.create(:cat_reading_wrangler, user: user)
-# 	FactoryBot.create(:contact, contactable: user)
-# end
-
-# 5.times do
-# 	library = FactoryBot.create(:library)
-# 	FactoryBot.create(:contact, contactable: library)
-# end
+5.times do
+	library = FactoryBot.create(:library)
+	FactoryBot.create(:contact, contactable: library)
+end
