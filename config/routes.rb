1ce85815835398d3e68a0f6235427615eb289ec4
BintLopez <nmlopez21@gmail.com>
2018-03-31 12:36:08 -0500

    clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
- #  devise_for :users
-	
-	# root to: "home#index"
-  
- #  get "home/index"
- #  match 'home', to: "home#index", via: :all
-
- #  namespace :dashboard do
- #  	resources :cats
-	# end
 	devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
