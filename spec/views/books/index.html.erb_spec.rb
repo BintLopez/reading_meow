63c3dfd3d0ac87fce1790cd884e93ccff9de55a0
BintLopez <nmlopez21@gmail.com>
2018-04-01 01:24:20 -0500

    oh em gee -- when doubles aren't worth it

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -3,20 +3,34 @@ require 'rails_helper'
 RSpec.describe "books/index", type: :view do
   before(:each) do
     assign(:books, [
-      Book.create!(
-        :author => "Author",
-        :title => "Title",
-        :library => nil,
-        :status => "Status",
-        :condition => "Condition"
+      instance_double(Book,
+        author: "Author",
+        title: "Title",
+        library: nil,
+        status: "Status",
+        condition: "Condition",
+        to_model: double(
+          persisted?: true,
+          model_name: double(
+            name: "Book",
+            singular_route_key: 'book'
+          )
+        ),
       ),
-      Book.create!(
-        :author => "Author",
-        :title => "Title",
-        :library => nil,
-        :status => "Status",
-        :condition => "Condition"
-      )
+      instance_double(Book,
+        author: "Author",
+        title: "Title",
+        library: nil,
+        status: "Status",
+        condition: "Condition",
+        to_model: double(
+          persisted?: true,
+          model_name: double(
+            name: "Book",
+            singular_route_key: 'book'
+          )
+        ),
+      ),
     ])
   end
 
