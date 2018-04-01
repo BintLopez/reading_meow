240dbd76bef5249124b399c076de12b7ddc69c29
BintLopez <nmlopez21@gmail.com>
2018-04-01 01:30:39 -0500

    change hash formatting in test

--- spec/views/books/show.html.erb_spec.rb
+++ spec/views/books/show.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe "books/show", type: :view do
   before(:each) do
     @book = assign(:book, instance_double(Book,
-      :author => "Author",
-      :title => "Title",
-      :library => instance_double(Library),
-      :status => "Status",
-      :condition => "Condition"
+      author: "Author",
+      title: "Title",
+      library: instance_double(Library),
+      status: "Status",
+      condition: "Condition",
     ))
   end
 
