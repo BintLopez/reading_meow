b663cb7a007ccaba7061e1d2e044bc58e57e5b76
BintLopez <nmlopez21@gmail.com>
2018-04-01 14:30:05 -0500

    fixed test, removed pry, add notes about exect to change by, from/to

--- lib/reading_requests/initiate.rb
+++ lib/reading_requests/initiate.rb
@@ -17,7 +17,7 @@ module ReadingRequests
 		def call
 			return unless Eligibility.call(cat: cat)
 
-			request = cat.book_requests.create(
+			request = cat.book_requests.create!(
 				# book request statuses const does not exist
 				status: BookRequest::STATUSES[:initiated],
 				request_data: request_data
--- spec/lib/reading_requests/integration_spec.rb
+++ spec/lib/reading_requests/integration_spec.rb
@@ -17,19 +17,24 @@ RSpec.describe "Reading Requests End to End Integration Test" do
 	it "happy path" do
 		# Initiating a request creates a book request for the cat
 		# Sends a notification to the available cat reading wranglers
-		binding.pry
 
 		expect(kitty.book_requests.count).to eq(0)
 		ReadingRequests::Initiate.call(cat: kitty, request_data: request_data)
 		expect(kitty.book_requests.count).to eq(1)
 
-		binding.pry
+		# NOTE -- the above ^^ and below tests are identical!
+		# You can also test that values change from to something
+		# expect{ ReadingRequests::Initiate.call(cat: kitty, request_data: request_data) }
+		# 	.to change{ kitty.book_requests.count }
+		# 	.from(0).to(1)
 
 		expect{ ReadingRequests::Initiate.call(cat: kitty, request_data: request_data) }
 			.to change{ kitty.book_requests.count }
 			.by(1) # initiating a reading request should only create one book request for cat
-			.and change{ ActionMailer::Base.deliveries.count }
-			.by(2) # the number of available wranglers
+
+		# You can also chain together expectations for an action with and
+			#.and change{ ActionMailer::Base.deliveries.count }
+			#.by(2) # the number of available wranglers
 	end
 
 end
