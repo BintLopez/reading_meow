8d9bacd25fb79a506439e0a503a5d4a71fb21c98
BintLopez <nmlopez21@gmail.com>
2018-04-02 14:59:32 -0500

    failing tests for shoulda matcher practice exercises

--- spec/models/cat_reading_wrangler_spec.rb
+++ spec/models/cat_reading_wrangler_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CatReadingWrangler, type: :model do
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:library) }
   it { is_expected.to validate_presence_of(:library_card_number) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_one(:cat) }
+  it { is_expected.to have_one(:cat_reading_wrangler) }
+  it { is_expected.to have_many(:contacts) }
 end
