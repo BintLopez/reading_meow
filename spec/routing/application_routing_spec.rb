f6232c83f9b3427c69fb184bdd00c5620759cfd6
BintLopez <nmlopez21@gmail.com>
2018-04-02 17:09:49 -0500

    add pending application routing spec

---
+++ spec/routing/application_routing_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe ApplicationController, type: :routing do
+  describe "Basic application routing" do
+    pending "Implement me to test drive your routes!"
+  end
+end
