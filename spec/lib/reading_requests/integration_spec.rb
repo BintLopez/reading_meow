41061a99d07a0abc84cf6b90ce297f0810640c77
BintLopez <nmlopez21@gmail.com>
2018-04-01 21:38:10 -0500

    add failing tests for ReadingRequests::Checkout -- fix all the bugs in your code to make them pass

--- spec/lib/reading_requests/integration_spec.rb
+++ spec/lib/reading_requests/integration_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 require 'reading_requests/initiate'
 require 'reading_requests/accept'
+require 'reading_requests/checkout'
 
 RSpec.describe "Reading Requests End to End Integration Test" do
 	# Note -- we can use a lazy let here because we will
@@ -23,6 +24,14 @@ RSpec.describe "Reading Requests End to End Integration Test" do
 	let!(:wrangler_1) { FactoryBot.create(:cat_reading_wrangler) }
 	let!(:wrangler_2) { FactoryBot.create(:cat_reading_wrangler) }
 
+	let(:library) { FactoryBot.create(:library) }
+
+	let(:book_1) { FactoryBot.create(:book) }
+	let(:book_2) { FactoryBot.create(:book) }
+	let(:book_3) { FactoryBot.create(:book) }
+
+	let!(:books) { [book_1, book_2, book_3] }
+
 	it "happy path" do
 		# Initiating a request creates a book request for the cat
 		# Sends a notification to the available cat reading wranglers
@@ -34,9 +43,20 @@ RSpec.describe "Reading Requests End to End Integration Test" do
 
 		current_request = kitty.current_book_request
 
+		# Accepting a request associates the request to the wrangler
 		expect{ ReadingRequests::Accept.call(initiated_request: current_request, wrangler: wrangler_1) }
-			.to change{ wrangler_1.reload.book_requests.count }
+			.to change{ wrangler_1.book_requests.count }
 			.by(1)
+
+		# Checking out books for the request creates a checkout
+		expect{ ReadingRequests::Checkout.call(request: current_request, books: books, library: library) }
+			.to change{ current_request.checkout }
+			.from(nil)
+			.to an_instance_of(Checkout)
+
+		checkout = current_request.checkout
+		expect(checkout.books.count).to eq(3)
+		expect(checkout.books).to match_array(books)
 	end
 
 end
