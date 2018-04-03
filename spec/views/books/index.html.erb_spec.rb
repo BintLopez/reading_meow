0ad929b0dd5152247e5e590c3fa9e28cbf3e8fea
BintLopez <nmlopez21@gmail.com>
2018-04-02 22:17:13 -0500

    Issuing my verdict -- not worth it

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "books/index", type: :view do
+  let(:library) { FactoryBot.create(:library) }
+  let!(:passing_attributes) do
+    {
+      author:    "Author",
+      title:     "Title",
+      library:   library,
+      status:    "Status",
+      condition: "Condition"
+    }
+  end
+
   before(:each) do
     assign(:books, [
-      FactoryBot.create(:book),
-      FactoryBot.create(:book)
+      FactoryBot.create(:book, passing_attributes),
+      FactoryBot.create(:book, passing_attributes)
     ])
   end
 
@@ -12,7 +23,7 @@ RSpec.describe "books/index", type: :view do
     render
     assert_select "tr>td", :text => "Author".to_s, :count => 2
     assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil, :count => 2
     assert_select "tr>td", :text => "Status".to_s, :count => 2
     assert_select "tr>td", :text => "Condition".to_s, :count => 2
   end
