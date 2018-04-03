3226402edca258dbe286e38281f2c816822e80a6
BintLopez <nmlopez21@gmail.com>
2018-04-03 06:10:16 -0500

    fix conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-<<<<<<< HEAD
-require 'support/factory_bot' # must be required _after_ spec_helper
-=======
 require 'support/factory_bot'
 require 'support/faker'
->>>>>>> set_up_with_rspec_and_friends_complete
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
