c203eb04d80d82b5dc254dcdf38073811d8a2c40
BintLopez <nmlopez21@gmail.com>
2018-04-02 21:05:57 -0500

    book spec failure

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   it 'has a valid factory' do
-    factory = FactoryBot.create
+    factory = FactoryBot.create(:book)
     expect(factory).to be_valid
     expect(factory).to be_persisted
   end
