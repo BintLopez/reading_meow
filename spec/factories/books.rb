6340852903c39c42ba6d3c7068ed8a31a9881c8f
BintLopez <nmlopez21@gmail.com>
2018-04-02 20:45:28 -0500

    fix book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :book do
-    author "MyString"
-    title "MyString"
-    library nil
-    status "MyString"
-    condition "MyString"
+    author    { Faker::Book.author }
+    title     { Faker::Book.author }
+    library   { Library.new }
+    status    "checked_out"
+    condition "new"
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  it 'has a valid factory' do
+    factory = FactoryBot.create
+    expect(factory).to be_valid
+    expect(factory).to be_persisted
+  end
 end
