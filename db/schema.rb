19a83451350fbcb38d01b5eaa55fd3681bc1d10d
BintLopez <nmlopez21@gmail.com>
2018-04-15 23:18:59 -0500

    correct cats migration

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20180330195906) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
+    t.string "breed"
     t.text "bio"
     t.integer "user_id"
+    t.boolean "profile_public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cats_on_user_id"
