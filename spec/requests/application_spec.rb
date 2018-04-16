4677507064d77d835e0b4f1e0c77d632c3415868
BintLopez <nmlopez21@gmail.com>
2018-04-16 15:35:08 -0400

    add pending test in spec/requests

---
+++ spec/requests/application_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Application", type: :request do
+  describe "GET /home" do
+    it "successfully gets the home page" do
+      skip "Implement me to test your web requests!"
+    end
+  end
+end
